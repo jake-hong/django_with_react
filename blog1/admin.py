377b3f36b7eb3519857960391cb75d743297f552
jake-hong <hongse21@gmail.com>
2021-12-23 19:45:16 +0900

    Add insta app + admin 기능 확인

--- blog1/admin.py
+++ blog1/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+# 등록법1
+# admin.site.register(Post)
+
+# 등록법2
+# class PostAdmin(admin.ModelAdmin):
+#    pass
+
+# 등록법3
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
